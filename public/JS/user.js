e1f4517398245cdf76ad7c6d360e816e77b0c2e5
vitornascimentto <vitornascimentto0@gmail.com>
2019-03-27 22:02:23 -0300

    Inserção no Banco de Dados

--- public/JS/user.js
+++ public/JS/user.js
@@ -40,4 +40,49 @@ class User {
         });
     }
 
+    async adicionar(){        
+        // Crinça
+        let docRefCrianca = await this.DB.firestore().collection('crianca').add({
+            name: localStorage.getItem('name'),
+            dateNascimento: new Date(localStorage.getItem('dataNascimento'))
+            //sexo: sexoCria
+        });
+
+        console.log(docRefCrianca.id);
+
+        // Responsável
+        let docRefResponsavel = await self.DB.firestore().collection('responsável').add({
+            pai: localStorage.getItem('pai'),
+            mae: localStorage.getItem('mae'),
+            outros: localStorage.getItem('outros'),
+            endereco: {
+                bairro: localStorage.getItem('bairro'),
+                rua: localStorage.getItem('rua'),
+                numero: Number(localStorage.getItem("numero")),
+                pontoReferencia: localStorage.getItem("pontoReferencia"),
+                enderecoAlternativo: localStorage.getItem("enderecoAlternativo"),
+            }, 
+            telefone: localStorage.getItem('telefone')
+        });
+
+        console.log(docRefResponsavel.id);
+
+        // Ocorrência
+        await self.DB.firestore().collection('ocorrencia').add({
+            dataAtendimento: new Date(localStorage.getItem('dataAtendimento')),
+            description: localStorage.getItem('descricao'),
+            numeroRegistro: Number(localStorage.getItem('numeroRegistro')),
+            observacoes: localStorage.getItem('observacoes'),
+            
+            reference_crianca: {
+                id: docRefCrianca.id,
+                reference: docRefCrianca
+            },
+            reference_responsavel: {
+                id: docRefResponsavel.id,
+                refence: docRefResponsavel
+            }
+        });
+    }
+
 }
